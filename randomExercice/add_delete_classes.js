99b14780a1f2e360aab134ffb80322bb810fbd0e
Devibtissam <dev.ibtissamelhammoumy@gmail.com>
2022-03-15 00:02:52 +0100

    --

--- randomExercice/add_delete_classes.js
+++ randomExercice/add_delete_classes.js
@@ -1,16 +1,16 @@
 const inputs = document.querySelectorAll('input');
 const parentClassAdded = document.querySelector('.classes-list div');
+const classText = [];
+const removedClasses = [];
 
-console.log(inputs);
-
-function addClasses(classvalue) {
+function sortClasses(classvalue) {
   const classArr = classvalue.split(' ').sort();
+  // assign the class values to our array
   for (let i = 0; i < classArr.length; i++) {
-    const createdElt = document.createElement('span');
-    createdElt.classList.add(classArr[i]);
-    createdElt.innerHTML = `${classArr[i]}`;
-    parentClassAdded.appendChild(createdElt);
+    classText.push(classArr[i]);
   }
+  // sort the list of classes name in alphabetically order
+  return classText.sort();
 }
 
 inputs.forEach((input) => {
@@ -18,11 +18,29 @@ inputs.forEach((input) => {
     const input = e.currentTarget;
     const inputType = e.currentTarget.getAttribute('class');
     const inputTypeValue = e.currentTarget.value.toLowerCase();
-    console.log(inputTypeValue);
+
     // check which  input user typed
     if (inputType === 'classes-to-add') {
-      addClasses(inputTypeValue);
+      const textClasses = sortClasses(inputTypeValue);
+
+      // to remove elt and start sorting it from over
+      while (parentClassAdded.firstChild) {
+        parentClassAdded.firstChild.remove();
+      }
+      for (let i = 0; i < textClasses.length; i++) {
+        const createdElt = document.createElement('span');
+        createdElt.classList.add(textClasses[i]);
+        createdElt.innerHTML = `${textClasses[i]}`;
+        parentClassAdded.appendChild(createdElt);
+      }
       input.value = '';
+    } else if (inputType === 'classes-to-remove') {
+      const childrenElt = parentClassAdded.children;
+      for (const childElt of childrenElt) {
+        if (childElt.classList.contains(inputTypeValue)) {
+          childElt.remove();
+        }
+      }
     }
   });
 });
